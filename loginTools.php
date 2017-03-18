3d1a02c6b5268f544d07708aabbcba0c89f76f35
Christopher Birmingham <chris.birmingham@hotmail.co.uk>
2017-03-18 19:28:29 +0000

    Adding code to contect to the db

--- loginTools.php
+++ loginTools.php
@@ -12,10 +12,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = ((isset($_POST['usernameInput'])) ? $_POST['usernameInput'] : '');
     $password = ((isset($_POST['passwordInput'])) ? $_POST['passwordInput'] : '');
 
-    $_SESSION['loggedIn'] = true;
-    //echo $username . '<br>';
-    //echo $password . '<br>';
-    header('Location: /hack24/home.php');
+    $db = new mysqli('localhost', 'php', 'php', 'hack24');
+
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
+
+    $result = $db->query('select * from users where username = "' . $username . '" and password = "' . $password . '"');
+
+    if ($result->num_rows === 1) {
+        $_SESSION['loggedIn'] = true;
+        $_SESSION['username'] = $username;
+        header('Location: /hack24/home.php');
+    } else {
+        header('Location: /hack24/homepage.html');
+    }
+
 } else {
-    header('Location: /hack24/index.php');
+    header('Location: /hack24/homepage.html');
 }
